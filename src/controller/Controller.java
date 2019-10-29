91a41107a3401780a1877f9546dfc2769611b059
unknown <ne.calero@uniandes.edu.co>
2019-10-29 10:21:23 -0600

    Se conecta el menu de MVCView con Controller, estos se conectan con Modelo

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -27,6 +27,7 @@ public class Controller {
 	{
 		Scanner lector = new Scanner(System.in);
 		boolean fin = false;
+		modelo = new MVCModelo();
 		while( !fin ){
 			view.printMenu();
 
@@ -35,9 +36,6 @@ public class Controller {
 			{
 			case 1:
 				view.printMessage("Se estan cargando los viajes: ");
-				modelo = new MVCModelo();
-				
-				
 				try
 				{
 					modelo.cargar();
@@ -50,24 +48,68 @@ public class Controller {
 				break;
 
 			case 2:
+				view.printMessage("Ingrese la cantidad de letras a consultar.");
+				short N = (short) lector.nextInt();
+				String[] respuesta1A = modelo.reqFunc1A(N);
+				
 				
 			case 3:
+				view.printMessage("Ingrese la latitud");
+				double lat2a = lector.nextDouble();
+				view.printMessage("Ingrese la longitud");
+				double lon2a = lector.nextDouble();
+				String[] respuesta2A = modelo.reqFunc2A(lat2a,lon2a);
 				
 			case 4:
+				view.printMessage("Ingrese un limite bajo de segundos");
+				double limMin3A = lector.nextDouble();
+				view.printMessage("Ingrese un limite alto de segundos");
+				double limMax3A = lector.nextDouble();
+				view.printMessage("Ingrese la cantidad de resultados que desea mostrar");
+				short N3A = (short) lector.nextDouble();
+				String[] respuesta3A = modelo.reqFunc3A(limMin3A, limMax3A, N3A);
 				
 			case 5:
+				view.printMessage("Ingrese la cantidad de resultados que desea mostrar");
+				short N1B = (short) lector.nextDouble();
+				String[] respuesta1B = modelo.reqFunc1B(N1B);
 				
 			case 6:
+				view.printMessage("Ingrese la latitud");
+				double lat2B = lector.nextDouble();
+				view.printMessage("Ingrese la longitud");
+				double lon2B = lector.nextDouble();
+				String[] respuesta2B = modelo.reqFunc2B(lat2B,lon2B);
 				
 			case 7:
+				view.printMessage("Ingrese el limite bajo de la desviacion estandar");
+				double LimBajo3B = lector.nextInt();
+				view.printMessage("Ingrese el limite alto de la desviacion estandar");
+				double LimAlto3B = lector.nextDouble();
+				
+				String[] respuesta3B = modelo.reqFunc3B(LimBajo3B, LimAlto3B);
 				
 			case 8:
+				view.printMessage("Ingrese el ID de la zona de salida");
+				int ID1C = lector.nextInt();
+				view.printMessage("Ingrese la hora");
+				double hora1C = lector.nextDouble();
+				String[] respuesta1C = modelo.reqFunc1C(ID1C, hora1C);
 			
 			case 9:
+				view.printMessage("");
+				short N = lector.nextInt();
+				String[] respuesta2C = modelo.reqFunc2C(N);
 				
 			case 10:
+				view.printMessage("");
+				short N = lector.nextInt();
+				String[] respuesta3C = modelo.reqFunc3C(N);
 				
 			case 11:
+				view.printMessage("");
+				short N = lector.nextInt();
+				String[] respuesta4C = modelo.reqFunc4C(N);
 			
 			default: 
 				view.printMessage("--------- \n Opcion Invalida !! \n---------");
